d2e42344d3683b6177fb4bafb5ac405469e779fc
hasibulislam999 <hasib143sl@gmail.com>
2022-07-01 23:29:47 +0600

    route included on app.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+import Todo from './routes/Todo';
+import MyTasks from './routes/MyTasks';
+import CompleteTasks from './routes/CompleteTasks';
+import Calender from './routes/Calender';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Todo />} />
+        <Route path='/todo' element={<Todo />} />
+        <Route path='/myTasks' element={<MyTasks />} />
+        <Route path='/completeTasks' element={<CompleteTasks />} />
+        <Route path='/calender' element={<Calender />} />
+      </Routes>
+      <Footer />
+    </React.Fragment>
   );
 }
 
