af2774cd75ab16de04e564746eeb0e18ea9cd17d
hasibulislam999 <hasib143sl@gmail.com>
2022-07-02 00:19:22 +0600

    indicator for routes

---
+++ src/components/CustomLink.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+    Link,
+    useMatch,
+    useResolvedPath,
+} from "react-router-dom";
+
+function CustomLink({ children, to, ...props }) {
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+
+    return (
+        <div
+            style={{
+                margin: "0 .5rem",
+            }}
+        >
+            <Link
+                style={
+                    {
+                        fontWeight: match ? "bold" : "normal" ,
+                        color: "white",
+                        textDecoration: "none"
+                    }
+                }
+                to={to}
+                {...props}
+            >
+                {children}
+            </Link>
+        </div>
+    );
+}
+
+export default CustomLink;
