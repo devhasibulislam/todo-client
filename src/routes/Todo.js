d296da3096d54629b814b09837bca9c3652a3f6c
hasibulislam999 <hasib143sl@gmail.com>
2022-07-02 00:21:38 +0600

    todo routing & styling done

---
+++ src/routes/Todo.js
@@ -0,0 +1,77 @@
+import axios from 'axios';
+import React from 'react';
+import '../styles/todo.css';
+import { useQuery } from 'react-query';
+
+const Todo = () => {
+    const handleTodoCreate = async (event) => {
+        event.preventDefault();
+
+        const todoTitle = event.target.title?.value;
+        const todoDesc = event.target.desc?.value;
+
+        const { data } = await axios.post('http://localhost:5000/todo', {
+            title: todoTitle,
+            desc: todoDesc,
+            state: 'uncompleted'
+        });
+
+        refetch();
+        console.log(data);
+        event.target.reset();
+    };
+
+    const { data: todoLists, refetch } = useQuery('todoLists', () => fetch('http://localhost:5000/todo').then(res => res.json()));
+
+    return (
+        <React.Fragment>
+            <section className='todo-grid'>
+                <div>
+                    <form onSubmit={handleTodoCreate}>
+                        <div className='form-block'>
+                            <input
+                                type="text"
+                                name="title"
+                                className="todo-title"
+                                placeholder='todo title'
+                                required
+                            />
+                        </div>
+                        <div className='form-block'>
+                            <textarea
+                                name="desc"
+                                className="desc"
+                                rows="3"
+                                placeholder='todo description'
+                                required
+                            ></textarea>
+                        </div>
+                        <div className='form-block'>
+                            <input type="submit" value="Add todo" className='todo-btn' />
+                        </div>
+                    </form>
+                </div>
+                <div>
+                    <div>
+                        <h3>Latest tasks</h3>
+                        {
+                            todoLists?.map(todoList => <p key={todoList._id}>{todoList.title}</p>)
+                        }
+                    </div>
+                    <div>
+                        <h3>Last completed tasks</h3>
+                        {
+                            todoLists.map(todoList => {
+                                if(todoList.state === 'completed'){
+                                    return <p style={{textDecoration: "line-through"}}>{todoList.title}</p>
+                                }
+                            })
+                        }
+                    </div>
+                </div>
+            </section>
+        </React.Fragment>
+    );
+};
+
+export default Todo;
